68ba43f27de1959a82629842a5d756383d541924
Semelion <senya2708@gmail.com>
2023-03-31 15:58:13 +0300

    qqq

--- trafnyak_s_l/prj.test/arrayt_test.cpp
+++ trafnyak_s_l/prj.test/arrayt_test.cpp
@@ -3,6 +3,8 @@
 
 #include <arrayt/arrayt.hpp>
 
+#include <cmath>
+
 using arrayd = ArrayT<double>;
 
 TEST_CASE("[arrayd] - arrayd ctor") {
@@ -13,7 +15,26 @@ TEST_CASE("[arrayd] - arrayd ctor") {
   arrayd test(10);
   test.resize(15);
   CHECK(test.size() == 15);
+  test.insert(3,1.0);
+  CHECK(test[3] == 1.0);
+  CHECK(test.size() == 16);
+
 
   CHECK_THROWS(test[-1]);
+  test.resize(10);
   CHECK_THROWS(test[20]);
+}
+
+TEST_CASE("[arrayd] - arrayd2") {
+  arrayd mass1(10);
+
+  for(int i = 0; i < mass1.size(); i++){
+    mass1[i] = 100/i - 12*i + std::pow(2, i);
+  }
+
+  arrayd mass2 = mass1;
+
+  for(int i = 0; i < mass2.size(); i++){
+    CHECK(mass1[i] == mass2[i]);
+  }
 }
