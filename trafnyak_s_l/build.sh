2b3377a063f738e7280ce991048440eb5fc5b1ae
user <user2023@MacBook-Air-user.local>
2023-04-08 12:03:50 +0300

    Changes to be committed: changed bash file for make builds on ubuntu and mac 	modified:   build.sh

--- trafnyak_s_l/build.sh
+++ trafnyak_s_l/build.sh
@@ -1,6 +1,12 @@
+
 rm -rf bin/
 rm -rf build/
 
-cmake -S . -B build -DCMAKE_TOOLCHAIN_FILE=/opt/vcpkg/scripts/buildsystems/vcpkg.cmake
-cmake --build build --config Release
+if [[ $uname = "Linux" ]]
+then
+  cmake -S . -B build -DCMAKE_TOOLCHAIN_FILE=/opt/vcpkg/scripts/buildsystems/vcpkg.cmake
+else
+  cmake -B build -S . "-DCMAKE_TOOLCHAIN_FILE=/Users/user2023/vcpkg/scripts/buildsystems/vcpkg.cmake"
+fi
 
+cmake --build build --config Release
