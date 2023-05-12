edd98f9e9d3eca1a9edbd60538775f093a5d68b2
Semelion <senya2708@gmail.com>
2023-05-12 13:37:10 +0300

    new file:   trafnyak_s_l/prj.lab/orto/CMakeLists.txt 	new file:   trafnyak_s_l/prj.lab/orto/main.cpp

---
+++ trafnyak_s_l/prj.lab/orto/main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <string.h>
+namespace geo2d{
+	namespace orto{
+		class quad {
+		public:
+			quad() = default;
+			quad(const quad&) = default;
+			~quad() = default;
+		};
+		class rect : quad{
+		public:
+			rect(const double x_start_in, const double y_start_in, const double x_end_in, const double y_end_in) {
+			    x_start = x_start_in;
+				x_end = x_end_in;
+				y_start = y_start_in;
+				y_end = y_end_in;
+			}
+			std::string get_value(){
+
+				std::string out = "(" + std::to_string(x_start) + ", " + std::to_string(y_start);
+				return out;
+			}
+		private:
+			double x_start = 0;
+			double x_end = 0;
+			double y_start = 0;
+			double y_end = 0;
+		};
+	}
+}
+
+int main(){
+	geo2d::orto::rect test(1,1,2,2);
+	std::cout << test.get_value() << "\n";
+	return 0;
+}
