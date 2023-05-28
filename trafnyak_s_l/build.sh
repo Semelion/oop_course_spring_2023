9253cc773b4e124d8722746c1b6242a3bbe9b8e2
Semelion <senya2708@gmail.com>
2023-05-28 11:48:45 +0300

    add course work

--- trafnyak_s_l/build.sh
+++ trafnyak_s_l/build.sh
@@ -1,8 +1,8 @@
-rm -rf bin/
+#rm -rf bin/
 rm -rf build/
 
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-	cmake -S . -B build -DCMAKE_TOOLCHAIN_FILE=/opt/vcpkg/scripts/buildsystems/vcpkg.cmake
+	cmake -B build -S . -DCMAKE_TOOLCHAIN_FILE=/home/semelion/vcpkg/scripts/buildsystems/vcpkg.cmake
 else
 	cmake -B build -S . -DCMAKE_TOOLCHAIN_FILE=/Users/user2023/vcpkg/scripts/buildsystems/vcpkg.cmake
 fi
--- trafnyak_s_l/old_libs/rational/build.sh
+++
@@ -1,6 +0,0 @@
-rm -rf bin/
-rm -rf build/
-
-cmake -S . -B build -DCMAKE_TOOLCHAIN_FILE=/opt/vcpkg/scripts/buildsystems/vcpkg.cmake
-cmake --build build --config Release
-
